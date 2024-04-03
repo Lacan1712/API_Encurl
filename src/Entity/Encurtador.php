9b786906e53e376ca9e159e41b4475772fac7617
Rodrigo Lacan Barbosa Costa <lacan.xrl80@gmail.com>
2024-04-03 12:03:36 -0400

    Bug no fluxo de constructor
    Construct ainda adicionava o link mesmo sem estar na lista de domínios.

--- src/Entity/Encurtador.php
+++ src/Entity/Encurtador.php
@@ -24,18 +24,20 @@ class Encurtador{
         if(filter_var($long_link, FILTER_VALIDATE_URL)){
             
 
-            $domain = parse_url($this->long_link, PHP_URL_HOST);
+            $domain = parse_url($long_link, PHP_URL_HOST);
             $list_domains = new Domains;
 
-            if($list_domains->getDomainsByName($domain) == true){
+            if($list_domains->getDomainsByName($domain) != true){
                 $this->long_link   = $long_link;
                 $this->permanencia = $permanencia;
             }
+               $this->long_link = null;
+               $this->short_link = null;
 
-            array_push($this->data['ERROR'], 'ERROR NON LISTED DOMAIN');
-            $message = implode(',',$this->data['ERROR']);
-            putLog("Erro em Entity Encurtador (construct) ".$message,'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_entity.txt');
-
+                array_push($this->data['ERROR'], 'ERROR NON LISTED DOMAIN');
+                $message = implode(',',$this->data['ERROR']);
+                putLog("Erro em Entity Encurtador (construct) ".$message,'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_entity.txt');
+            
 
         }else{
             
@@ -55,12 +57,22 @@ class Encurtador{
         $randomString = md5(uniqid());
         $numericOnly = preg_replace('/[0-9]/','',$randomString);
         $threeDigits = substr($numericOnly,0,5);
-        $this->short_link = 'SB'.$threeDigits;
+        $this->short_link = strval($threeDigits);
         //Verificação de duplicidade
-        $consulta = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
-                    ->query('SELECT long_link FROM dbo.ENCURTADO');
+        /*$consulta = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
+                    ->query("INSERT INTO dbo.ENCURTADO(short_link,long_url) VALUES ($this->short_link, $this->long_link, $this->permanencia)");
+        $stmt = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
+                        ->prepare("INSERT INTO dbo.ENCURTADO (short_link, long_link, permanencia) VALUES (:short_link, :long_link, :permanencia)");
+
+        // Vincular os valores da variável
+        $stmt->bindParam(':short_link', $this->short_link);
+        $stmt->bindParam(':long_link', $this->long_link);
+        $stmt->bindParam(':permanencia', $this->permanencia);
         
-        return $consulta;
+        $stmt->execute();*/
+
+        echo $this->long_link;
+    
         
 
 
@@ -86,8 +98,5 @@ foreach ($consulta as $linha) {
     print_r($linha);
 }*/
 
-$encurtador = new Encurtador('https://www.youtube.com/watch?v=z5VC9Wnd8Wo&list=PLXik_5Br-zO8vLD6X9uB-EH6BpgZL8XBH&index=3');
+$encurtador = new Encurtador('',0);
 $result = $encurtador->encurtar();
-foreach($result as $r){
-    echo $r[0];
-}
