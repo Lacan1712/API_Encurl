e21725859ab608be37fdd46cc6922c4082549724
Rodrigo Lacan Barbosa Costa <lacan.xrl80@gmail.com>
2024-04-03 17:18:13 -0400

    Adicionando lógica de desencurtar
    Adicionando o processo de entity com a lógica de desencurtar.

--- src/Entity/Desencurtador.php
+++ src/Entity/Desencurtador.php
@@ -2,23 +2,55 @@
 namespace APP\Entity\Desencurtador;
 require 'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Database.php';
 require 'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\APP\Common\Environment.php';
+require_once 'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\PutLogs.php';
 
 //require 'C:\xampp\htdocs\API DATABASE\vendor\autoload.php';
 use Database\Connection\database as DB;
+use Exception;
+use PDO;
+
 class Desencurtador{
-    
+   
+    public $short_link;
+    public $desativado;
+
+    public function desencurtar($short_link,$desativado){
+        
+        $this->short_link = $short_link;
+        $this->desativado = $desativado;
+        $db_conn = new DB;
+        if($this->desativado == 1){
+            try{
+
+                $stmt = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
+                        ->prepare("DELETE FROM dbo.ENCURTADO WHERE short_link = ?");
+                $stmt->bindParam(1,$this->short_link);
+                $stmt->execute();
+
+
+            }catch(Exception $e){
+                return false;
+                putLog("Erro em Entity Desencurtador (desencurtar) ".$e,'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_entity.txt');
+            }
+        }
+
+
+        if($this->desativado == 0){
+        
+            $stmt = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
+                            ->prepare("SELECT long_link FROM dbo.ENCURTADO WHERE short_link = ?");
+            $stmt->bindParam(1,$this->short_link);
+            $stmt->execute();
+
+            $long_link = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $long_link[0];
+        }
+        
+        
+    }
     
 
 }
 
-
-$database_connection = new DB('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt');
-
-$consulta= $database_connection
-            ->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
-                ->query("SELECT * FROM ENCURTADOS");
-
-
-foreach ($consulta as $linha) {
-    print_r($linha);
-}
+$desencurtar = new Desencurtador;
+print_r($desencurtar->desencurtar('cecbc',0));
--- src/Entity/Encurtador.php
+++ src/Entity/Encurtador.php
@@ -8,6 +8,8 @@ require_once 'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\PutLogs.php'
 use Database\Connection\database as DB;
 use Exception;
 use src\Include\Domains;
+use PDO;
+
 
 class Encurtador{
     //Atributos
@@ -15,7 +17,9 @@ class Encurtador{
     public $short_link;
     public $permanencia;
     public $data= [
-        'ERROR'=> []
+        'ERROR'=> [],
+        'ALERT'=>[],
+        'RESPONSE'=> []
     ];
 
 
@@ -27,16 +31,18 @@ class Encurtador{
             $domain = parse_url($long_link, PHP_URL_HOST);
             $list_domains = new Domains;
 
-            if($list_domains->getDomainsByName($domain) != true){
+            if($list_domains->getDomainsByName($domain) == true){
                 $this->long_link   = $long_link;
                 $this->permanencia = $permanencia;
             }
-               $this->long_link = null;
-               $this->short_link = null;
 
+            if($list_domains->getDomainsByName($domain) == false){
                 array_push($this->data['ERROR'], 'ERROR NON LISTED DOMAIN');
                 $message = implode(',',$this->data['ERROR']);
                 putLog("Erro em Entity Encurtador (construct) ".$message,'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_entity.txt');
+            }  
+
+                
             
 
         }else{
@@ -59,27 +65,41 @@ class Encurtador{
         $threeDigits = substr($numericOnly,0,5);
         $this->short_link = strval($threeDigits);
         //Verificação de duplicidade
-        /*$consulta = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
-                    ->query("INSERT INTO dbo.ENCURTADO(short_link,long_url) VALUES ($this->short_link, $this->long_link, $this->permanencia)");
         $stmt = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
-                        ->prepare("INSERT INTO dbo.ENCURTADO (short_link, long_link, permanencia) VALUES (:short_link, :long_link, :permanencia)");
+                        ->prepare("SELECT short_link,long_link FROM dbo.ENCURTADO WHERE long_link = ?");
 
-        // Vincular os valores da variável
-        $stmt->bindParam(':short_link', $this->short_link);
-        $stmt->bindParam(':long_link', $this->long_link);
-        $stmt->bindParam(':permanencia', $this->permanencia);
+        $stmt->bindParam(1,$this->long_link);
+        $stmt->execute();
+        $verify_long_link = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        $stmt->execute();*/
+        if($verify_long_link){
+            array_push($this->data['ALERT'], ['LINK_ALREDY_EXIST' => $verify_long_link['short_link']]);
+        }else{
 
-        echo $this->long_link;
-    
-        
+            $stmt = $db_conn->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
+                            ->prepare("INSERT INTO dbo.ENCURTADO (short_link, long_link, permanencia) VALUES (:short_link, :long_link, :permanencia)");
 
+            //Vincular os valores da variável
+            $stmt->bindParam(':short_link', $this->short_link);
+            $stmt->bindParam(':long_link', $this->long_link);
+            $stmt->bindParam(':permanencia', $this->permanencia);
+            
+            $stmt->execute();
+
+            array_push($this->data['RESPONSE'],[
+                'SHORT_LINK' => $this->short_link,
+                'LONG_LINK' => $this->long_link,
+                'PERMANENCIA'=>$this->permanencia
+            ]);
+
+        }       
 
     }catch(Exception $erro){
         $mensagem_erro = "Erro na entidade Encurtar (encurtar): ". $erro->getMessage();
         $path_log = 'C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_entity.txt';
         putLog($mensagem_erro,$path_log);        
+    }finally{
+        $db_conn = null;
     }
         
     }
@@ -88,15 +108,11 @@ class Encurtador{
 }
 
 
-/*$database_connection = new DB();
-$consulta= $database_connection
-            ->dbConn('C:\xampp\htdocs\API ENCURTADOR\API_Encurl\src\Include\Log_database.txt')
-                ->query("SELECT * FROM ENCURTADOS");
 
-
-foreach ($consulta as $linha) {
-    print_r($linha);
-}*/
-
-$encurtador = new Encurtador('',0);
+$encurtador = new Encurtador('https://sebraepr-my.sharepoint.com/:b:/r/personal/dayana_palhano_rr_sebrae_com_br1/Documents/CARTILHA%20LGPD%20VF.pdf?csf=1&web=1&e=kbiwPa',0);
 $result = $encurtador->encurtar();
+
+if($encurtador->data){
+    echo "<pre>";
+    print_r($encurtador->data);
+}
